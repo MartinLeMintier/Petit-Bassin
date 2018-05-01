8c0f1c3c28440bdbe27ab8a327eb17a6a41b484b
QuentinAre <quentin.are@edu.ece.fr>
2018-05-01 12:06:53 +0200

    fonctionnement connection

--- traitementdebut.php
+++ traitementdebut.php
@@ -4,7 +4,9 @@ if(isset($_GET['username']) AND isset($_GET['email']) AND isset($_GET['choix']))
 {
 	if(empty($_GET['username']) OR empty($_GET['email']) OR empty($_GET['choix']))
 	{
-		echo 'Erreur un champ est vide!';
+		?> <p> Erreur un champ est vide! <p> <?php
+		?><a href="Connexion.php">retourner au menu principale</a><?php
+		exit;
 	}
 	else
 	{		
@@ -18,25 +20,23 @@ if(isset($_GET['username']) AND isset($_GET['email']) AND isset($_GET['choix']))
 			$bdd = new PDO('mysql:host=localhost;dbname=petit_bateau;charset=utf8', 'root', '');
 			$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$monom = $bdd->prepare('SELECT * FROM administrateur');
-			$monom->execute(array($username, $adresse_mail));
+			$monom->execute(array($adresse_mail, $username));
 			
 		}
 		catch(Exception $e)
 		{
-			
 			// En cas d'erreur, on affiche un message et on arrête tout
-				die('Erreur : '.$e->getMessage());
+			die('Erreur : '.$e->getMessage());
 		}
-		
-		if($monom)
-		{
 
-			if($choix=='Admin')
+		if($choix=='Admin')
+		{
+				
+			while ($donnees = $monom->fetch())
 			{
-						
-				while ($donnees = $monom->fetch())
+				
+				if($donnees['Adresse_mail']==$adresse_mail)
 				{
-					
 					if($donnees['mdp']==$username)
 					{					
 						?>
@@ -44,25 +44,31 @@ if(isset($_GET['username']) AND isset($_GET['email']) AND isset($_GET['choix']))
 							<strong>Admin</strong> : <?php echo $donnees['nom']; ?><br />
 							l'amin est : <?php echo $donnees['prenom']; ?>, et son adresse mail est <?php echo $donnees['Adresse_mail']; ?> !<br />
 						   </p>
+						   
 						<?php
 						
+						exit;
 					}
-						
+					else
+					{
+						?> <p> mdp incorrecte <p> <?php
+						?><a href="Connexion.php">retourner au menu principale</a><?php
+						exit;
+					}
 				}
-				
-			}
-			else
-			{
-			?> <p> Pas acces <p> <?php
 			}
 		}
-		if(!$monom)
-			echo 'Pas dans la base de donnee';
+		else
+		{
+			?> <p> Pas acces <p> <?php
+		}
+		
 		// On affiche chaque entrée une à une
 		//$reponse->closeCursor(); // Termine le traitement de la requête
-		
 	}
 }
 
-$monom->closeCursor();
+?> <p> adresse mail inexistante chez les administrateurs <p> <?php
+?><a href="Connexion.php">retourner au menu principale</a><?php
+
 ?>
