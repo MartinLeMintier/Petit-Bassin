1e0a03c780a8b86e2c41cd9dbe27fc74c47cccd2
SoleneConsten <solene.consten@edu.ece.fr>
2018-05-04 19:21:05 +0200

    Afficher amis + reseau pro

--- AfficherCollegues.php
+++ AfficherCollegues.php
@@ -1,38 +1,40 @@
 <?php
 
 session_start();
-unset ($_SESSION['nomami1']);
-unset ($_SESSION['nomami2']);
-unset ($_SESSION['nomami3']);
-unset ($_SESSION['nomami4']);
-unset ($_SESSION['nomami5']);
-unset ($_SESSION['nomami6']);
-unset ($_SESSION['nomami7']);
-unset ($_SESSION['nomami8']);
-unset ($_SESSION['nomami9']);
-unset ($_SESSION['nomami10']);
 
-unset ($_SESSION['prenomami1']);
-unset ($_SESSION['prenomami2']);
-unset ($_SESSION['prenomami3']);
-unset ($_SESSION['prenomami4']);
-unset ($_SESSION['prenomami5']);
-unset ($_SESSION['prenomami6']);
-unset ($_SESSION['prenomami7']);
-unset ($_SESSION['prenomami8']);
-unset ($_SESSION['prenomami9']);
-unset ($_SESSION['prenomami10']);
+unset ($_SESSION['nomcollegue1']);
+unset ($_SESSION['nomcollegue2']);
+unset ($_SESSION['nomcollegue3']);
+unset ($_SESSION['nomcollegue4']);
+unset ($_SESSION['nomcollegue5']);
+unset ($_SESSION['nomcollegue6']);
+unset ($_SESSION['nomcollegue7']);
+unset ($_SESSION['nomcollegue8']);
+unset ($_SESSION['nomcollegue9']);
+unset ($_SESSION['nomcollegue10']);
 
-unset ($_SESSION['photopp1']);
-unset ($_SESSION['photopp2']);
-unset ($_SESSION['photopp3']);
-unset ($_SESSION['photopp4']);
-unset ($_SESSION['photopp5']);
-unset ($_SESSION['photopp6']);
-unset ($_SESSION['photopp7']);
-unset ($_SESSION['photopp8']);
-unset ($_SESSION['photopp9']);
-unset ($_SESSION['photopp10']);
+unset ($_SESSION['prenomcollegue1']);
+unset ($_SESSION['prenomcollegue2']);
+unset ($_SESSION['prenomcollegue3']);
+unset ($_SESSION['prenomcollegue4']);
+unset ($_SESSION['prenomcollegue5']);
+unset ($_SESSION['prenomcollegue6']);
+unset ($_SESSION['prenomcollegue7']);
+unset ($_SESSION['prenomcollegue8']);
+unset ($_SESSION['prenomcollegue9']);
+unset ($_SESSION['prenomcollegue10']);
+
+unset ($_SESSION['photoCpp1']);
+unset ($_SESSION['photoCpp2']);
+unset ($_SESSION['photoCpp3']);
+unset ($_SESSION['photoCpp4']);
+unset ($_SESSION['photoCpp5']);
+unset ($_SESSION['photoCpp6']);
+unset ($_SESSION['photoCpp7']);
+unset ($_SESSION['photoCpp8']);
+unset ($_SESSION['photoCpp9']);
+unset ($_SESSION['photoCpp10']);
+
 try
 		{
 			// On se connecte à MySQL
@@ -47,148 +49,152 @@ try
 			// En cas d'erreur, on affiche un message et on arrête tout
 			die('Erreur : '.$e->getMessage());
 		}
+
 		$incremente=0;
 		$utili = $_SESSION['ID'];
-		$marequete = $bdd->prepare('SELECT * FROM amis WHERE ID= ?');
+		$marequete = $bdd->prepare('SELECT * FROM reseaupro WHERE ID= ?');
 		$marequete->execute(array($utili));
+		echo $utili;
 
 		if(!$marequete)
 		{
 			echo 'RIEN';
 			exit;
 		}
-
-		else {
-
-
-		$marequete2 = $bdd->prepare('SELECT IDami FROM amis WHERE ID= ?');
+         else {
+		$marequete2 = $bdd->prepare('SELECT IDcollegue FROM reseaupro WHERE ID= ?');
 		$marequete2->execute(array($utili));
 		$donnees2=$marequete2->fetch();
 
 		while($donnees=$marequete->fetch())
-		{
+		{ echo 'bisou';
 			if($donnees['ID']==$utili)
-			{
+			{ echo 'bisou';
 				$incremente=$incremente+1;
-				$ami=$donnees['IDami'];
+				$ami=$donnees['IDcollegue'];
 				$marequete3 = $bdd->prepare('SELECT nom FROM auteur WHERE ID= ?');
 				$marequete3->execute(array($ami));
 				while($donnees3=$marequete3->fetch(PDO::FETCH_ASSOC))
 				{
 					$tableau[]=$donnees3['nom'];
 					$truc=$donnees3['nom'];
-					echo 'truuuuuuc';
+					echo 'bisou';
 				}
 
 			}
 		}
-		echo $incremente;
+		//echo $incremente;
 		for($i=$incremente-1;$i>-1;$i--)
 		{
 			if($i==$incremente-1)
 			{
-				$_SESSION['monami1']=$tableau[$incremente-1];
+				$_SESSION['moncollegue1']=$tableau[$incremente-1];
 				$marequete4 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete4->execute(array($tableau[$incremente-1]));
 				$donnees4=$marequete4->fetch();
-				$_SESSION['prenomami1']=$donnees4['prenom'];
-				$_SESSION['photopp1']=$donnees4['photodepp'];
+				$_SESSION['prenomcollegue1']=$donnees4['prenom'];
+				$_SESSION['photoCpp1']=$donnees4['photodepp'];
 				echo $donnees4['prenom'];
+				echo $donnees4[$incremente-1];
 
 			}
 			if($i==$incremente-2)
 			{
-				$_SESSION['monami2']=$tableau[$incremente-2];
+				$_SESSION['moncollegue2']=$tableau[$incremente-2];
 				$marequete5 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete5->execute(array($tableau[$incremente-2]));
 				$donnees5=$marequete5->fetch();
-				$_SESSION['prenomami2']=$donnees5['prenom'];
-				$_SESSION['photopp2']=$donnees5['photodepp'];
+				$_SESSION['prenomcollegue2']=$donnees5['prenom'];
+				$_SESSION['photoCpp2']=$donnees5['photodepp'];
 
 				echo $donnees5['prenom'];
 			}
 			if($i==$incremente-3)
 			{
-				$_SESSION['monami3']=$tableau[$incremente-3];
+				$_SESSION['moncollegue3']=$tableau[$incremente-3];
 				$marequete6 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete6->execute(array($tableau[$incremente-3]));
 				$donnees6=$marequete6->fetch();
-				$_SESSION['prenomami3']=$donnees6['prenom'];
-				$_SESSION['photopp3']=$donnees6['photodepp'];
+				$_SESSION['prenomcollegue3']=$donnees6['prenom'];
+				$_SESSION['photoCpp3']=$donnees6['photodepp'];
 			}
 			if($i==$incremente-4)
 			{
-				$_SESSION['monami4']=$tableau[$incremente-4];
+				$_SESSION['moncollegue4']=$tableau[$incremente-4];
 				$marequete7 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete7->execute(array($tableau[$incremente-4]));
 				$donnees7=$marequete7->fetch();
-				$_SESSION['prenomami4']=$donnees7['prenom'];
-				$_SESSION['photopp4']=$donnees7['photodepp'];
+				$_SESSION['prenomcollegue4']=$donnees7['prenom'];
+				$_SESSION['photoCpp4']=$donnees7['photodepp'];
 			}
 			if($i==$incremente-5)
 			{
-				$_SESSION['monami5']=$tableau[$incremente-5];
+				$_SESSION['moncollegue5']=$tableau[$incremente-5];
 				$marequete8 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete8->execute(array($tableau[$incremente-5]));
 				$donnees8=$marequete8->fetch();
-				$_SESSION['prenomami5']=$donnees8['prenom'];
-				$_SESSION['photopp5']=$donnees8['photodepp'];
+				$_SESSION['prenomcollegue5']=$donnees8['prenom'];
+				$_SESSION['photoCpp5']=$donnees8['photodepp'];
 			}
 			if($i==$incremente-6)
 			{
-				$_SESSION['monami6']=$tableau[$incremente-6];
+				$_SESSION['moncollegue6']=$tableau[$incremente-6];
 				$marequete9 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete9->execute(array($tableau[$incremente-6]));
 				$donnees9=$marequete9->fetch();
-				$_SESSION['prenomami6']=$donnees9['prenom'];
-				$_SESSION['photopp6']=$donnees9['photodepp'];
+				$_SESSION['prenomcollegue6']=$donnees9['prenom'];
+				$_SESSION['photoCpp6']=$donnees9['photodepp'];
 			}
 			if($i==$incremente-7)
 			{
-				$_SESSION['monami7']=$tableau[$incremente-7];
+				$_SESSION['moncollegue7']=$tableau[$incremente-7];
 				$marequete10 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete10->execute(array($tableau[$incremente-7]));
 				$donnees10=$marequete10->fetch();
-				$_SESSION['prenomami7']=$donnees10['prenom'];
-				$_SESSION['photopp7']=$donnees10['photodepp'];
+				$_SESSION['prenomcollegue7']=$donnees10['prenom'];
+				$_SESSION['photoCpp7']=$donnees10['photodepp'];
 			}
 			if($i==$incremente-8)
 			{
-				$_SESSION['monami8']=$tableau[$incremente-8];
+				$_SESSION['moncollegue8']=$tableau[$incremente-8];
 				$marequete11 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete11->execute(array($tableau[$incremente-8]));
 				$donnees11=$marequete11->fetch();
-				$_SESSION['prenomami8']=$donnees11['prenom'];
-				$_SESSION['photopp8']=$donnees11['photodepp'];
+				$_SESSION['prenomcollegue8']=$donnees11['prenom'];
+				$_SESSION['photoCpp8']=$donnees11['photodepp'];
 			}
 			if($i==$incremente-9)
 			{
-				$_SESSION['monami9']=$tableau[$incremente-9];
+				$_SESSION['moncollegue9']=$tableau[$incremente-9];
 				$marequete12 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete12->execute(array($tableau[$incremente-9]));
 				$donnees12=$marequete12->fetch();
-				$_SESSION['prenomami9']=$donnees12['prenom'];
-				$_SESSION['photopp9']=$donnees12['photodepp'];
+				$_SESSION['prenomcollegue9']=$donnees12['prenom'];
+				$_SESSION['photoCpp9']=$donnees12['photodepp'];
 			}
 			if($i==$incremente-10)
 			{
-				$_SESSION['monami10']=$tableau[$incremente-10];
+				$_SESSION['moncollegue10']=$tableau[$incremente-10];
 				$marequete13 = $bdd->prepare('SELECT prenom, photodepp FROM auteur WHERE nom= ?');
 				$marequete13->execute(array($tableau[$incremente-10]));
 				$donnees13=$marequete13->fetch();
-				$_SESSION['prenomami10']=$donnees13['prenom'];
-				$_SESSION['photopp10']=$donnees13['photodepp'];
+				$_SESSION['prenomcollegue10']=$donnees13['prenom'];
+				$_SESSION['photoCpp10']=$donnees13['photodepp'];
 			}
 			
 		}
 		//echo $tableau[$incremente-2];
 		echo $tableau[$incremente-1];
 		
-		$_SESSION['NbAmis']=$incremente;
+		$_SESSION['NbColleges']=$incremente;
 
         
 
 
-		header('Location: AfficherCollegues.php');
+
+
+
+
+		header('Location: reseau.php');
 	}
 ?>
--- reseau.php
+++ reseau.php
@@ -49,29 +49,25 @@ session_start();
 
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><div class="col-md-4 col-md-offset-3">
-            <form action="" class="search-form">
-                <div class="form-group has-feedback">
-                <label for="search" class="sr-only">Recherche</label>
-                <input type="text" class="form-control" name="search" id="search" placeholder="Rechercher...">
-                  <span class="glyphicon glyphicon-search form-control-feedback"></span>
-              </div>  
-            </form>
-        </div></li>
+          <li><div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Rechercher..." name="search">
+      <button type="submit"><i class="glyphicon glyphicon-search"></i></button>
+    </form></li>
         </ul>
-      <!-- Icone pour la Notification -->
+        <!-- Icone pour la Notification -->
         <ul class="nav navbar-nav navbar-right">
           <li><a href="Connexion.php"><span class="glyphicon glyphicon-bell"></span> Notifications</a></li>
 
         </ul>
-      <!-- Icone pour la messagerie -->
+        <!-- Icone pour la messagerie -->
         <ul class="nav navbar-nav navbar-right">
           <li><a href="Connexion.php"><span class=" glyphicon glyphicon-envelope"></span> Messagerie</a></li>
 
         </ul>
-      <!-- Icone pour la Mon reseau -->
+        <!-- Icone pour la Mon reseau -->
         <ul class="nav navbar-nav navbar-right">
-          <li><a href="reseau.php"><span class="glyphicon glyphicon-link"></span> Mon Réseau</a></li>
+          <li><a href="AfficheAmis.php"><span class="glyphicon glyphicon-link"></span> Mon Réseau</a></li>
 
         </ul>
 
@@ -80,7 +76,7 @@ session_start();
           <li><a href="Profil.php"><span class="glyphicon glyphicon-user"></span> Vous</a></li>
 
         </ul>
-      <!-- Icone pour la acceuil -->
+        <!-- Icone pour la acceuil -->
         <ul class="nav navbar-nav navbar-right">
           <li><a href="AccueilUtilisateur.php"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
 
@@ -91,7 +87,7 @@ session_start();
       </div>
 
       
-        
+
       
 
     </div>
@@ -113,8 +109,8 @@ session_start();
 	<div id="myDiv1">
 		</div>
      
-     	<script>
-     	//<?php echo 'cacaprout';?>
+     	<!--<script>
+     	
 
 	    var nb= '<?php echo $_SESSION['NbAmis'];?>';
 	    var nb2= '<?php echo $_SESSION['monami1'];?>';
@@ -129,65 +125,199 @@ session_start();
 		
 		
 			
-		</script>
+		</script>-->
 		<script>
-     	//<?php echo 'cacaprout';?>
-	    var nb4= '<?php echo $_SESSION['monami4'];?>';
-		document.getElementById('myDiv1').innerHTML += '<p> '+ nb4 +'</p>';
-		
-		
-			
+     	
+	    var nom1= '<?php echo $_SESSION['monami1'];?>';
+	    var pp1 = '<?php echo $_SESSION['photopp1'];?>';
+	    var pre1 = '<?php echo $_SESSION['prenomami1'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp1 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom1 + ' ' + pre1 + ' </strong></div></div>';
+		</script>
+		<script >
+
+		var nom2= '<?php echo $_SESSION['monami2'];?>';
+	    var pp2 = '<?php echo $_SESSION['photopp2'];?>';
+	    var pre2 = '<?php echo $_SESSION['prenomami2'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp2 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom2 + ' ' + pre2 + ' </strong></div></div>';
+</script>
+<script>
+		var nom3= '<?php echo $_SESSION['monami3'];?>';
+	    var pp3 = '<?php echo $_SESSION['photopp3'];?>';
+	    var pre3 = '<?php echo $_SESSION['prenomami3'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp3 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom3 + ' ' + pre3 + ' </strong></div></div>';
 		</script>
 
+		<script>
+
+		var nom4= '<?php echo $_SESSION['monami4'];?>';
+	    var pp4 = '<?php echo $_SESSION['photopp4'];?>';
+	    var pre4 = '<?php echo $_SESSION['prenomami4'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp4 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom4 + ' ' + pre4 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+
+		var nom5= '<?php echo $_SESSION['monami5'];?>';
+	    var pp5 = '<?php echo $_SESSION['photopp5'];?>';
+	    var pre5 = '<?php echo $_SESSION['prenomami5'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp5 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom5 + ' ' + pre5 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+
+		var nom6= '<?php echo $_SESSION['monami6'];?>';
+	    var pp6 = '<?php echo $_SESSION['photopp6'];?>';
+	    var pre6 = '<?php echo $_SESSION['prenomami6'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp6 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom6 + ' ' + pre6 + ' </strong></div></div>';
+		</script>
+
+		<script>
+
+		var nom7= '<?php echo $_SESSION['monami7'];?>';
+	    var pp7 = '<?php echo $_SESSION['photopp7'];?>';
+	    var pre7 = '<?php echo $_SESSION['prenomami7'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp7 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom7 + ' ' + pre7 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+
+		var nom8= '<?php echo $_SESSION['monami8'];?>';
+	    var pp8 = '<?php echo $_SESSION['photopp8'];?>';
+	    var pre8 = '<?php echo $_SESSION['prenomami8'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp8 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom8 + ' ' + pre8 + ' </strong></div></div>';
+		</script>
+
+		<script>
+
+		var nom9= '<?php echo $_SESSION['monami9'];?>';
+	    var pp9 = '<?php echo $_SESSION['photopp9'];?>';
+	    var pre9 = '<?php echo $_SESSION['prenomami9'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp9 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom9 + ' ' + pre9 + ' </strong></div></div>';
+		</script>
+
+		<script>
+
+		var nom10= '<?php echo $_SESSION['monami10'];?>';
+	    var pp10 = '<?php echo $_SESSION['photopp10'];?>';
+	    var pre9 = '<?php echo $_SESSION['prenomami10'];?>';
+		document.getElementById('myDiv1').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + pp10 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nom10 + ' ' + pre10 + ' </strong></div></div>';
+		</script>
+			
 		
 
+		</div>
+		</div>
 
-	<!--<div class="col-sm-3">
-	<div class="well">
-          
-           
-          </div>
-	
-	</div>
 
-	<div class="col-sm-3">
-	<div class="well">
-	Ami2
-	</div>
-	
-	</div>
-
-	<div class="col-sm-3">
-	<div class="well">
-	Ami3
-	</div>
-	</div>
-
-	<div class="col-sm-3">
-	<div class="well">
-	Ami4
-	</div>
-	</div>
-
-	<div class="col-sm-3">
-	<div class="well">
-	Ami5
-	</div>
-	</div>
-</div>-->
+<div class="container-fluid">
+    <div class="reseau_pro">
 
 <div class="Page_Pro">
 	<div class="col-sm-12 titre-pro" id="Pro">
 
 	Mon Réseau Professionnel
 	</div>
-    <div class="liste">
+
+	<div id="myDiv2">
+	
+
+	</div>
+
+
+	<script>
+     	
+	    var nomC1= '<?php echo $_SESSION['moncollegue1'];?>';
+	    var ppC1 = '<?php echo $_SESSION['photoCpp1'];?>';
+	   
+	    var preC1 = '<?php echo $_SESSION['prenomcollegue1'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC1 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC1 + ' ' + preC1 + ' </strong></div></div>';
+		
+		</script>
+		<script>
+     	
+	    var nomC2= '<?php echo $_SESSION['moncollegue2'];?>';
+	    var ppC2 = '<?php echo $_SESSION['photoCpp2'];?>';
+	    var preC2 = '<?php echo $_SESSION['prenomcollegue2'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC2 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC2 + ' ' + preC2 + ' </strong></div></div>';
+		</script>
+<script>
+     	
+	    var nomC3= '<?php echo $_SESSION['moncollegue3'];?>';
+	    var ppC3 = '<?php echo $_SESSION['photoCpp3'];?>';
+	    var preC3 = '<?php echo $_SESSION['prenomcollegue3'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC3 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC3 + ' ' + preC3 + ' </strong></div></div>';
+		</script>
+
+		<script>
+     	
+	    var nomC4= '<?php echo $_SESSION['moncollegue4'];?>';
+	    var ppC4 = '<?php echo $_SESSION['photoCpp4'];?>';
+	    var preC4 = '<?php echo $_SESSION['prenomcollegue4'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC4 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC4 + ' ' + preC4 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+     	
+	    var nomC5= '<?php echo $_SESSION['moncollegue5'];?>';
+	    var ppC5 = '<?php echo $_SESSION['photoCpp5'];?>';
+	    var preC5 = '<?php echo $_SESSION['prenomcollegue5'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC5 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC5 + ' ' + preC5 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+     	
+	    var nomC6= '<?php echo $_SESSION['moncollegue6'];?>';
+	    var ppC6 = '<?php echo $_SESSION['photoCpp6'];?>';
+	    var preC6 = '<?php echo $_SESSION['prenomcollegue6'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC6 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC6 + ' ' + preC6 + ' </strong></div></div>';
+		</script>
+
+		<script>
+     	
+	    var nomC7= '<?php echo $_SESSION['moncollegue7'];?>';
+	    var ppC7 = '<?php echo $_SESSION['photoCpp7'];?>';
+	    var preC7 = '<?php echo $_SESSION['prenomcollegue7'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC7 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC7 + ' ' + preC7 + ' </strong></div></div>';
+		</script>
+		
+		<script>
+     	
+	    var nomC8= '<?php echo $_SESSION['moncollegue8'];?>';
+	    var ppC8 = '<?php echo $_SESSION['photoCpp8'];?>';
+	    var preC8 = '<?php echo $_SESSION['prenomcollegue8'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC8 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC8 + ' ' + preC8 + ' </strong></div></div>';
+		</script>
+
+		<script>
+     	
+	    var nomC9= '<?php echo $_SESSION['moncollegue9'];?>';
+	    var ppC9 = '<?php echo $_SESSION['photoCpp9'];?>';
+	    var preC9 = '<?php echo $_SESSION['prenomcollegue9'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC9 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC9 + ' ' + preC9 + ' </strong></div></div>';
+		</script>
+
+		<script>
+     	
+	    var nomC10= '<?php echo $_SESSION['moncollegue10'];?>';
+	    var ppC10 = '<?php echo $_SESSION['photoCpp10'];?>';
+	    var preC10 = '<?php echo $_SESSION['prenomcollegue10'];?>';
+		document.getElementById('myDiv2').innerHTML += '<div class="col-sm-3"><div class="well"><img class="img-circle" src="images/' + ppC10 + '" height="65" width="65" alt="Avatar"> <strong>   '+ nomC10 + ' ' + preC10 + ' </strong></div></div>';
+
+		</script>
+
+<div id="MyDiv2">
+	
+
+	</div>
+
+  <!-- <div class="liste">
+   
 	<div class="col-sm-3">
 	<div class="well">
-           collègue1
+           
           </div>
 	
-	</div>
+    </div>
 
 	<div class="col-sm-3">
 	<div class="well">
@@ -215,9 +345,12 @@ session_start();
 	</div>
 </div>
 
- </div>
- </div>  
-	
+ </div>-->
+ </div> 
+ </div> 
+	</div>
 </body>
+
+
 
 </html>
